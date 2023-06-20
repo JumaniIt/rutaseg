f73841f6a0b9554c6dccc9a140630d9932305ec6
Nei <nicolas.ortola@mercadolibre.com>
2023-06-20 19:54:48 -0300

    first commit

---
+++ src/main/java/com/jumani/rutaseg/RutasegApplication.java
@@ -0,0 +1,13 @@
+package com.jumani.rutaseg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutasegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutasegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jumani/rutaseg/RutasegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumani.rutaseg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutasegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
