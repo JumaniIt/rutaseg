830448557aa0f1c80c07e0cf9ea296b1e24164e5
Nei <nicolasortola91@gmail.com>
2023-06-25 21:41:51 -0300

    commit

--- src/test/java/com/jumani/rutaseg/filter/SessionFilterFunctionalTest.java
+++ src/test/java/com/jumani/rutaseg/filter/SessionFilterFunctionalTest.java
@@ -31,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 class SessionFilterFunctionalTest extends RutasegApplicationTests {
+
     private MockMvc mvc;
 
     @Autowired
