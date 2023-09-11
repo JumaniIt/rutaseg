8122a3c44cb050f32428808471dcafcd7ddea40d
martinjulian <martinjulianramos@gmail.com>
2023-09-10 22:07:13 -0300

    TESTS-03

--- src/test/java/com/jumani/rutaseg/domain/ArrivalDataTest.java
+++ src/test/java/com/jumani/rutaseg/domain/ArrivalDataTest.java
@@ -2,20 +2,36 @@ package com.jumani.rutaseg.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ArrivalDataTest {
     @Test
-    public void testCustomsData() {
+    public void testArrivalData() {
         // Arrange
-        String name = "John Doe";
-        String phone = "1234567890";
+        ArrivalData arrivalData = new ArrivalData(
+                LocalDate.of(2023, 9, 1),
+                LocalTime.of(10, 30),
+                Origin.EXOLGAN,
+                "Morning",
+                false,
+                DestinationType.TLEA,
+                "Warehouse A",
+                "FOB123",
+                "USD"
+        );
 
-        // Act
-        CustomsData customsData = new CustomsData(name, phone);
-
-        // Assert
-        assertEquals(name, customsData.getName());
-        assertEquals(phone, customsData.getPhone());
+        // Verificar que los valores se establecieron correctamente
+        assertEquals(LocalDate.of(2023, 9, 1), arrivalData.getArrivalDate());
+        assertEquals(LocalTime.of(10, 30), arrivalData.getArrivalTime());
+        assertEquals(Origin.EXOLGAN, arrivalData.getOrigin());
+        assertEquals("Morning", arrivalData.getTurn());
+        assertFalse(arrivalData.isFreeLoad());
+        assertEquals(DestinationType.TLEA, arrivalData.getDestinationType());
+        assertEquals("Warehouse A", arrivalData.getDestinationName());
+        assertEquals("FOB123", arrivalData.getFob());
+        assertEquals("USD", arrivalData.getCurrency());
     }
 }
